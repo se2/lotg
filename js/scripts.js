615cf37d0fa3855407a9f98beef2a4a96b5d464d
Tu Luong <luongduc.tu1992@gmail.com>
2017-07-11 00:05:05 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	$('.banner').slick({
+		slidesToShow: 1, 
+		slideToScroll: 1,
+		arrows: false,
+		dots: false,
+		infinite: true,
+		slide: '.slide',
+		autoplay: true,
+		autoplaySpeed: 6000,
+	});
+	
+});
